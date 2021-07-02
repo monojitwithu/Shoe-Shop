ee497aa599331a38c813ea4b9a90e30dbac7ddc6
monojitwithu <monojitwithu@gmail.com>
2021-07-02 22:06:14 +0530

    home Page Created

---
+++ src/components/section/Home.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+export class Home extends Component {
+    render() {
+        return (
+            <div>
+                
+                <img style={{width:"100%"}} src="https://digitalagencynetwork.com/wp-content/uploads/2018/11/brilliant-nike-campaigns.png"/>
+                <img  style={{height: "726px", width: "600px"}}src="https://i.pinimg.com/originals/10/8d/24/108d2489db3c373368bba55d11d20607.jpg"/>
+                <img  style={{width:"580px"}}src="https://img.buzzfeed.com/buzzfeed-static/static/2020-05/15/13/asset/5ddd925391f1/sub-buzz-872-1589548499-9.jpg?downsize=700%3A%2A&output-quality=auto&output-format=auto"/>
+               
+            </div>
+        )
+    }
+}
+
+export default Home
